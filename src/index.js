813d4e42643dbe21b2c46ef9c38e199af8fd9f70
Oksana Fedorenko <oksanafedorenko2016@gmail.com>
2020-08-19 22:17:03 +0200

    Added hotel description and user reviews

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ import "./img/favicon.png";
 import "./img/logo.png";
 import "./img/sprite.svg";
 import "./img/user.jpg";
-
+import "./img/hotel-1.jpg";
+import "./img/hotel-2.jpg";
+import "./img/hotel-3.jpg";
+import "./img/user-1.jpg";
+import "./img/user-2.jpg";
+import "./img/user-3.jpg";
+import "./img/user-4.jpg";
+import "./img/user-5.jpg";
+import "./img/user-6.jpg";
